405713f4c98571ae95186c239cb2af3311302d64
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-04 14:49:56 +0100

    mage can defend with pet

--- src/main/java/game_players/Mage.java
+++ src/main/java/game_players/Mage.java
@@ -14,4 +14,29 @@ public class Mage extends GamePlayers {
         super(name, weapon, inventory, health);
         this.pet = pet;
     }
+
+
+    public void defend(double damage) {
+        double health = this.getHealth();
+        double petHealth = this.pet.getHealth();
+
+
+        if (petHealth >= damage) {
+            petHealth -= damage;
+            damage = 0;
+        } else if(petHealth < damage){
+            damage -= petHealth;
+            petHealth = 0;
+        }
+
+        if (health >= damage) {
+            health -= damage;
+        } else if(health < damage){
+            health = 0;
+        }
+
+        setHealth(health);
+        pet.setHealth(petHealth);
+    }
+
 }
