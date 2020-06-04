b3e14e80359e973cca50bbc3e8bc2e21a3829beb
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-04 14:11:44 +0100

    refactors warrior test file to setup object prior

--- src/test/java/WarriorTest.java
+++ src/test/java/WarriorTest.java
@@ -2,6 +2,7 @@ import game_players.GamePlayers;
 import game_players.Warrior;
 import items.IItem;
 import items.Weapon;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,14 +11,23 @@ import static org.junit.Assert.assertEquals;
 
 public class WarriorTest {
 
+    private Weapon axe;
+    private GamePlayers player;
+    private ArrayList<IItem> inventory;
 
+    @Before
+    public void before(){
+        inventory = new ArrayList<IItem>();
+        axe = new Weapon("Axe", 10, 5);
+        player = new Warrior("Dave", axe, inventory, 100, 5);
+    }
 
     @Test
     public void canDefend(){
-        ArrayList<IItem> inventory  = new ArrayList<IItem>();
-        Weapon axe = new Weapon("Axe", 10, 5);
-        GamePlayers player = new Warrior("Dave", axe, inventory, 100, 5);
+
         player.defend(10);
         assertEquals(95, player.getHealth(), 0.01);
         }
+
+
 }
