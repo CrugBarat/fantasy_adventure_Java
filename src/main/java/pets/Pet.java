bb4d998fe3af63e6ca3d7b623ae1f862938c8fd8
CrugBarat <craigtbarratt@gmail.com>
2020-06-04 11:32:11 +0100

    adds Pet class with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/pets/Pet.java
@@ -0,0 +1,38 @@
+package pets;
+
+public class Pet {
+
+    private String name;
+    private String type;
+    private double health;
+
+    public Pet(String name, String type, double health) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+}
