d1cdf2f674986a1a73b3a85d1c74d4fd086eab97
CrugBarat <craigtbarratt@gmail.com>
2020-06-04 12:48:52 +0100

    refacs Room as composition with new vars/constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/rooms/Room.java
+++ src/main/java/rooms/Room.java
@@ -1,30 +1,51 @@
 package rooms;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import game_players.Enemy;
+import game_players.GamePlayers;
+
 import java.util.ArrayList;
 
 public class Room {
 
     private String name;
     private ArrayList<IItem> loot;
+    private ArrayList<Enemy> enemies;
+    private GamePlayers gamePlayer;
 
-    public Room(String name, ArrayList<IItem> loot) {
+    public Room(String name, GamePlayers gamePlayer) {
         this.name = name;
-        this.loot = loot;
+        this.loot = new ArrayList<>();
+        this.enemies = new ArrayList<>();
+        this.gamePlayer = gamePlayer;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getLootSize() {
+        return loot.size();
+    }
+
     public ArrayList<IItem> getLoot() {
         return loot;
+    }
+
+    public int getEnemiesSize() {
+        return enemies.size();
+    }
+
+    public ArrayList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public GamePlayers getGamePlayer() {
+        return gamePlayer;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setLoot(ArrayList<IItem> loot) {
-        this.loot = loot;
-    }
 }
