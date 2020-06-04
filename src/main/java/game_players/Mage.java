06ff3b28b5f47dfbc08f6bd16ea1b3cd41a540cd
CrugBarat <craigtbarratt@gmail.com>
2020-06-04 11:28:54 +0100

    adds Mage class with constructor
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/game_players/Mage.java
@@ -0,0 +1,15 @@
+package game_players;
+
+import items.Weapon;
+
+import java.util.ArrayList;
+
+public class Mage extends GamePlayers {
+
+    private Pet pet;
+
+    public Mage(String name, Weapon weapon, ArrayList<Weapon> inventory, double health, Pet pet) {
+        super(name, weapon, inventory, health);
+        this.pet = pet;
+    }
+}
